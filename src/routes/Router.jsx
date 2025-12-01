82fe56ab9109804d1804bd29f8598bbf31be680a
Sarvesh Ksahtriya <sarveshkshatriya545@gmail.com>
2025-12-02 01:25:53 +0530

    someting more specific changes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -18,10 +18,10 @@ export const router = createBrowserRouter(
       children: [
         {
           path: "/",
-          element: <Home></Home>,
+          element: <Home />,
         },
       ],
     },
-  ],
-  { basename: `/${repoName}` }
+  ]
 );
+
